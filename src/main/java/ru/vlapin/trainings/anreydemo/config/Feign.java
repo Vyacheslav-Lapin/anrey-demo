7a4351ed253417826a772aa6614721132038bb44
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2021-03-05 13:46:42 +0300

    Initial commit

---
+++ src/main/java/ru/vlapin/trainings/anreydemo/config/Feign.java
@@ -0,0 +1,13 @@
+package ru.vlapin.trainings.anreydemo.config;
+
+import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType.HAL;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+
+@Configuration
+@EnableHypermediaSupport(type = HAL)
+@EnableFeignClients("ru.vlapin.trainings.anreydemo.service")
+public class Feign {
+}
